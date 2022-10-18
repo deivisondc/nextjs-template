41871501d8ea2a6f1fcc519a1181c54339b99467
Deivison Cardoso <deivisondc@gmail.com>
2022-10-18 20:58:20 -0300

    add chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import { ChakraProvider, localStorageManager } from '@chakra-ui/react';
+
+import { theme } from '../styles/theme';
+
+import '../styles/globals.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	if (typeof window !== 'undefined') {
+		localStorage.setItem('chakra-ui-color-mode', 'dark');
+	}
+
+	return (
+		<ChakraProvider theme={theme} colorModeManager={localStorageManager}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 }
 
 export default MyApp;
